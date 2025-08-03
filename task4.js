13ea00fbe4502896b7a983360d1973ad7ca9a683
ROKSANA DILSHAD <lagonlegend847@gmail.com>
2025-08-03 15:52:07 +0600

    task4.js Add

---
+++ task4.js
@@ -0,0 +1,14 @@
+let marks = 165;
+
+if(marks >= 80){
+    console.log("your gread is: A+ ");
+}
+else if(marks >= 70){
+    console.log("your gread is: A ");
+}
+else if(marks >= 60){
+    console.log("your gread is: A- ");
+}
+else{
+    console.log("your gread is: Fail ");
+}
